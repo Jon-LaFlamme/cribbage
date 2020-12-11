187b2ba7266b445468f08bab649d43fe51990c6f
Jon-LaFlamme <jonlaflamme7@gmail.com>
2020-12-11 18:58:33 -0500

    learning peg sequence

--- learning.py
+++ learning.py
@@ -7,7 +7,23 @@ suits = ['clubs','diamonds','hearts','spades']
 values = {'ace':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,
             'nine':9,'ten':10,'jack':10,'queen':10,'king':10}
 
-#template: {"hand_id": {"peg_pts":int, "hand_points":int, "crib_pts":+/-int, "times_played":int. "avg_score":+/-int, }, ... }
+#template: {"hand_id": 
+#               {"times_played":int,
+#                "times_dealer":int,
+#                "neg_peg_pts_cum":-int,
+#                "pos_peg_pts_cum":int,
+#                "hand_points_cum":int, 
+#                "neg_crib_pts_cum":-int,
+#                "pos_crib_pts_cum"+int}, 
+#           ... }
+#notes: averages for different stats can be computed at runtime based on the situation
+# TODO(Jon) Optional: Make 'insane' difficulty, which stores the performance of each hand against every other hand
+# TODO(Jon) Optional: Also with 'insane' difficulty, store id as turncard_rank + played_cards_concatenated as rank_id_string
+#           Computer can loop through each card in hand to find matching ids and select best option
+#           Example: {"jacacetwothrfou": 3}
+#expected about 50,000 unique IDs after many iterations.
+
+
 performance_by_hand = {}
 dealer_performance_by_hand = {}
 
@@ -34,6 +50,43 @@ def peg_logic(hand_playing,stack,count,turncard):
     h = hand.Hand(hand_playing,turncard)
     return h.peg_selection(stack,count,turncard)
 
+def can_play(hand,count):
+    for card in hand:
+        if card.value + count <= 31:
+            return True
+    return False
+
+def peg_sequence(is_dealer_p1,turncard,p1,p2):
+    hand1 = p1.hand.copy()
+    hand2 = p2.hand.copy()
+    while hand1 or hand2:
+        count = 0
+        stack = []
+        p1_turn = True
+        while count <= 31 and (can_play(hand1,count) or can_play(hand2,count)):
+            if p1_turn and can_play(hand1,count):
+                choice = peg_logic(hand1,stack,count,turncard)
+                count += choice.value
+                stack.append(choice)
+                hand1.remove(choice)
+                p1_turn = False
+            if can_play(hand2,count):
+                choice = peg_logic(hand2,stack,count,turncard)
+                count += choice.value
+                stack.append(choice)
+                hand2.remove(choice)
+                p1_turn = True
+            
+
+
+                
+
+
+
+
+
+
+
 
 p1 = players.computer('difficult')
 p2 = players.computer('difficult')
@@ -50,12 +103,21 @@ for round in range(num_rounds):
         hand2.append(d.deal_one())
     p1_options = list(combinations(hand1,4))
     p2_options = list(combinations(hand2,4))
+    p1.cut_deck(d)
+    turncard = d.deck[0]
     for p1_hand,p2_hand in zip(p1_options,p2_options):
+        crib = []
         p1.hand = p1_hand
         p2.hand = p2_hand
+        #extract the crib
+        for card in hand1:
+            if card not in p1.hand:
+                crib.append(card)
+        for card in hand2:
+            if card not in p2.hand:
+                crib.append(card)
         #TODO(Jon) Create the following gameplay sequences
-        crib = discard_sequence(p1,p2)
-        peg_sequence(p1,p2)
+        peg_sequence(is_dealer_p1,turncard,p1,p2)
         show_sequence(is_dealer_p1,d,crib,p1,p2)
 
 
